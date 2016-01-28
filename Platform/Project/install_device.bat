855894ecc4277c22830199c9483d02fc9544e754
栗永春 <codefarmer3650@qq.com>
2016-01-28 15:25:51 +0800

    Modify the file directory and dependencies
    Modify the file directory and dependencies

--- Platform/Project/install_device.bat
+++ Platform/Project/install_device.bat
@@ -1,6 +1,11 @@
 xcopy build_device\ble_device_platform.hex ..\Image\ /F /Y
+
 xcopy ..\Image\ble_device_radio.hex  ..\..\Product\sdk\images\ /F /Y
 xcopy ..\Image\ble_device_platform.hex  ..\..\Product\sdk\images\ /F /Y
+xcopy ..\Image\flash-ble-device-system.bat	..\..\Product\sdk\images\ /F /Y
 
-xcopy ..\Image\ble_device_radio.hex  ..\..\basic_image_download\common\ /F /Y
-xcopy ..\Image\ble_device_platform.hex  ..\..\basic_image_download\common\ /F /Y
+xcopy ..\Image\ble_device_radio.hex  ..\..\BoardConfig\Image\common\ /F /Y
+xcopy ..\Image\ble_device_platform.hex  ..\..\BoardConfig\Image\common\ /F /Y
+xcopy ..\Image\flash-ble-device-system.bat  ..\..\BoardConfig\Image\common\ /F /Y
+
+
